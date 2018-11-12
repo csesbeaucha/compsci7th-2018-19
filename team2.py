f8076fbf49b8a74e3e961ca413b9c4b28f9513e7
J.C <joshuapetrofsky@gmail.com>
2018-11-12 14:00:48 -0600

    update

--- team2.py
+++ team2.py
@@ -7,6 +7,7 @@
 ####
 
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'the second team' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
