2867c3bed7b5f0bceac38378b023f313784aabfd
ganielsen <44908347+ganielsen@users.noreply.github.com>
2018-11-12 14:01:57 -0600

    Update team1.py

--- team1.py
+++ team1.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'team blue' # Only 10 chars displayed.
+team_name = 'team blu' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
