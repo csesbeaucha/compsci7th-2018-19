d6df7902ae6a179329009cb6f56ce8fe1b834eb9
Conspires <44908236+Conspires@users.noreply.github.com>
2018-11-16 14:12:19 -0600

    Description Change

--- team11.py
+++ team11.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Conspiring Against You' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'The Conspiring Strat'
+strategy_description = 'Conspire against others.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
