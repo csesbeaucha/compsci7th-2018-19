934ea2ae4df6a78248e74b9f010001e2791d663a
J.C <joshuapetrofsky@gmail.com>
2018-11-19 13:46:53 -0600

    a

--- team2.py
+++ team2.py
@@ -42,17 +42,24 @@ def move(my_history, their_history, my_score, their_score):
         return 'c'
     else:
         return 'b'
-     '''   
-        
-    #THE TESTED STRATEGY
+    '''
+    #COLLUDE
     
+    return 'c'
+    
+    #BETRAY
+    '''
+    
+    '''
+    #THE TESTED STRATEGY
+    '''
     if my_history=='' and their_history=='':
         return 'b'
     elif 'c' in their_history:
         return 'b'
     else:
         return 'c'
-    
+    '''
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
