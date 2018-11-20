16cb6f8ca0f10a1910d9d0a3d42ea3816d89a3a6
Alex Zhang <azhang08@claytonschools.net>
2018-11-20 13:28:04 -0600

    Update team5.py

--- team5.py
+++ team5.py
@@ -25,8 +25,9 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    if their_history -1== 'b':
+    if their_history==' ':
+        return 'b'
+    elif their_history[-1]== 'b':
         return 'b'
     else:
         return 'c'
